45c01e32bb9b5bbc9d048c9bb7c510b4d6e5483f
Andrey Krivtsov <Andrey.Krivtsov@softline.com>
2020-11-03 00:18:58 +0300

    Добавлена функция для ежедневного обновления всех сконфигурирвоанных сертификатов

--- LECertManager/LECertificateManager.cs
+++ LECertManager/LECertificateManager.cs
@@ -36,26 +36,6 @@ namespace LECertManager
             this.settings = options.Value;
         }
 
-        protected async Task<KeyVaultCertificateWithPolicy> GetCertificateInternal(string name)
-        {
-            if(string.IsNullOrWhiteSpace(name))
-                throw new RequestProcessingException("Certificate name can't be empty!", HttpStatusCode.BadRequest);
-            
-            var certInfo = settings.Certificates.FirstOrDefault(x => x.Name == name);
-            if (certInfo == null)
-                throw new RequestProcessingException($"Certificate {name} not found in the configuration!", HttpStatusCode.NotFound);
-
-            var cert = await keyVaultService.GetCertificateAsync(certInfo.KvCertName, certInfo.KeyVault.Uri);
-            if (cert == null)
-            {
-                logger.LogError("Certificate {certificateName} found, but can't get it from the KeyVault!", name);
-                
-                throw new RequestProcessingException($"Can't get certificate from the KeyVault", HttpStatusCode.ServiceUnavailable);
-            }
-
-            return cert;
-        }
-
         /// <summary>
         /// Получить данные сертификата, управляемого приложением, из KeyVault
         /// URL для вызова: GET  https://function.application.uri/api/certificates/{name}
@@ -136,18 +116,71 @@ namespace LECertManager
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "certificates/{name}")]
             HttpRequest req, string name)
         {
+            try
+            {
+                const string forceParam = "force";
+                bool isForce = false;
+
+                if (req.Query.ContainsKey(forceParam) && !bool.TryParse(req.Query[forceParam], out isForce))
+                    throw new RequestProcessingException($"Invalid value of the query string \"force\" parameter!", HttpStatusCode.BadRequest);
+                
+                var newCert = await UpdateCertificateInternalAsync(name, isForce);
+
+                if (newCert == null)
+                    return new NoContentResult();
+
+                return new ObjectResult(new CertificateDto(name, newCert)) {StatusCode = (int) HttpStatusCode.OK};
+            }
+            catch (RequestProcessingException e)
+            {    
+                return new ObjectResult(e.Message) {StatusCode = (int) e.StatusCode};
+            }
+        }
+        
+        /// <summary>
+        /// Регулярная проверка всех сертификатов и обновление просроченных или близких к этому (за 20 дней, настраивается
+        /// в конфигурации)
+        /// Вызывается в 2:15 каждый день
+        /// </summary>
+        /// <param name="myTimer"></param>
+        /// <returns></returns>
+        [FunctionName("UpdateAllCertificates")]
+        public async Task UpdateAllCertificates(
+            [TimerTrigger("0 15 2 * * *")] TimerInfo myTimer)
+        {
+            foreach (var certInfo in settings.Certificates)
+            {
+                try
+                {
+                    logger.LogInformation("Interval Check & Update certificate {certificateName}", certInfo.Name);
+
+                    await UpdateCertificateInternalAsync(certInfo.Name, false);
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, $"Error during update certificate {certInfo.Name}");
+                }
+            }
+            
+            logger.LogInformation("Interval certificates update completed!");
+        }
+
+        /// <summary>
+        /// Реализация обновления сертификата в KeyVault.
+        /// Если force == false, то перед обновлением проверяется дата истечения существующего сертификата
+        /// и сертификат обновляется только если он истек или истечет в течении 20 дней (параметр в кон-ии)  
+        /// </summary>
+        /// <param name="name">Имя сертификата в конфигурации</param>
+        /// <param name="isForce">Если true - принудительно обновлять</param>
+        /// <returns>Новый сертификат (объект KeyVaultCertificateWithPolicy) либо null, если не обновился</returns>
+        /// <exception cref="RequestProcessingException">В случае проверяемых ошибок</exception>
+        protected async Task<KeyVaultCertificateWithPolicy> UpdateCertificateInternalAsync(string name, bool isForce = false)
+        {
             var certInfo = settings.Certificates.FirstOrDefault(x => x.Name.Equals(name));
             if (certInfo == null)
-                return new NotFoundObjectResult($"Certificate {name} not found in configuration!");
-
-            const string forceParam = "force";
-            bool isForce = false;
-
-            if (req.Query.ContainsKey(forceParam) && !bool.TryParse(req.Query[forceParam], out isForce))
-            {
-                return new BadRequestObjectResult($"Invalid value of the query string \"force\" parameter!");
-            }
-
+                throw new RequestProcessingException($"Certificate {name} not found in configuration!", HttpStatusCode.NotFound);
+            
+            //Проверить, нужно ли заменять существующий сертификат, если не установлен флаг Force 
             if (!isForce)
             {
                 var oldCert = await keyVaultService.GetCertificateAsync(certInfo.KvCertName, certInfo.KeyVault.Uri);
@@ -155,11 +188,9 @@ namespace LECertManager
                 {
                     logger.LogInformation("Certificate {certificateName} found, expiration date: {expirationData}", 
                         name, oldCert.Properties.ExpiresOn);
-                    
+
                     if(!IsExpired(oldCert.Properties.ExpiresOn,TimeSpan.FromDays(settings.RenewalBeforeExpireDays)))
-                    {
-                        return new NoContentResult();           
-                    }
+                        return null;
                 }
             }
 
@@ -174,8 +205,34 @@ namespace LECertManager
 
             //Получить его из KV обратно
             var newCert = await GetCertificateInternal(name);
+
+            return newCert;
+        }
+        
+        /// <summary>
+        /// Реализация получения сертификата из KeyVault
+        /// </summary>
+        /// <param name="name">Имя сертификата в конфигурации</param>
+        /// <returns></returns>
+        /// <exception cref="RequestProcessingException">Выявленные ошибки в процессе</exception>
+        protected async Task<KeyVaultCertificateWithPolicy> GetCertificateInternal(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+                throw new RequestProcessingException("Certificate name can't be empty!", HttpStatusCode.BadRequest);
+            
+            var certInfo = settings.Certificates.FirstOrDefault(x => x.Name == name);
+            if (certInfo == null)
+                throw new RequestProcessingException($"Certificate {name} not found in the configuration!", HttpStatusCode.NotFound);
+
+            var cert = await keyVaultService.GetCertificateAsync(certInfo.KvCertName, certInfo.KeyVault.Uri);
+            if (cert == null)
+            {
+                logger.LogError("Certificate {certificateName} found, but can't get it from the KeyVault!", name);
                 
-            return new ObjectResult(new CertificateDto(name, newCert)) {StatusCode = (int)HttpStatusCode.OK};
+                throw new RequestProcessingException($"Can't get certificate from the KeyVault", HttpStatusCode.ServiceUnavailable);
+            }
+
+            return cert;
         }
 
         protected static bool IsExpired(DateTimeOffset? expTime, TimeSpan buffer)
